c3102580cd0257aa8ed3b096af1a8946a1718c26
PALASH <63140586+PALASH-BAJPAI@users.noreply.github.com>
2020-09-03 18:22:31 +0530

    Newsletter that can be used in different websites.

---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res) {
+
+    const firstName = req.body.fname;
+    const lastName = req.body.lname;
+    const email = req.body.email;
+
+    const data = {
+        members: [{
+            email_address: email,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: firstName,
+                LNAME: lastName
+            }
+        }]
+    }
+
+    const jsonData = JSON.stringify(data);
+    const url = "https://us10.api.mailchimp.com/3.0/lists/f6abf8b5c0";
+    const options = {
+        method: "POST",
+        auth: "palash:3806201d6c1e74376518ff3490d5896a-us10"
+    }
+
+    const request = https.request(url, options, function(response) {
+
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+
+        response.on("data", function(data) {
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
+
+});
+
+app.post("/failure", function(req, res) {
+    res.redirect("/");
+})
+
+app.listen(process.env.PORT||3000, () => console.log("server running on 3000"));
